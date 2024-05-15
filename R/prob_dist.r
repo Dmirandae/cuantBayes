1b34d027e9f3de540f9155dc7f8010ad31fc10d5
dmirandae <dmirandae@gmail.com>
2024-05-14 21:54:46 -0500

    2024 05 12 review

---
+++ R/prob_dist.r
@@ -0,0 +1,70 @@
+
+## Normal
+
+## density
+
+?dnorm()
+
+dnorm(1)
+
+## quantile
+
+
+## random
+
+rnorm(n=10)
+
+rnorm(n=10)
+
+set.seed(666)
+
+
+rnorm(n=10)
+
+rnorm(n=10)
+
+rnorm(n=10, mean = 0, sd = 1)
+
+
+## histogram
+
+myLovedNormalData <- rnorm(n=1000)
+
+hist(myLovedNormalData)
+
+
+hist(rnorm(n=1000))
+
+
+## sampling 
+
+par(mfrow=c(2,2))
+
+hist(rnorm(n=1000000))
+
+hist(rnorm(n=10000))
+
+hist(rnorm(n=1000))
+
+hist(rnorm(n=10))
+
+dev.off()
+
+## help
+
+help("distribution")
+?"distribution"
+??"distribution"
+
+## Poisson
+
+## Beta
+
+## Gamma
+
+## uniform
+
+## exponential
+
+##
+
